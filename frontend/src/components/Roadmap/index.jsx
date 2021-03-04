227ab7df0bf540e95dfb9d6afd4b13c84d7987b3
Isaac Frank <masturisaacfrank@gmail.com>
2021-03-04 21:50:30 +0100

    done

--- frontend/src/components/Roadmap/index.jsx
+++ frontend/src/components/Roadmap/index.jsx
@@ -4,7 +4,7 @@ const Roadmap = (props) => {
   return (
     <div className="text-center pb-1" id="roadmap">
       <h1 className="pt-2 pb-2">Roadmap</h1>
-      <img src="roadmap.png" alt="RoadMap"></img>
+      <img src="roadmap.png" alt="RoadMap" style={{maxWidth:"80%"}}></img>
     </div>
   );
 };
--- frontend/src/components/Tokenomics/index.jsx
+++ frontend/src/components/Tokenomics/index.jsx
@@ -3,7 +3,7 @@ const Tokenomics = (props) => {
   return (
     <div className="text-center pb-2 ml-3 mr-3" id="tokenomic">
       <h1 className="pt-2 pb-2">Tokenomics</h1>
-      <img src="tokenomics.png" alt="pie chart" />
+      <img src="tokenomics.png" alt="pie chart" style={{ maxWidth: "50%" }} />
       <div className="container d-flex justify-content-end">
         <b>
           <i>Max Supply : 100000</i>
